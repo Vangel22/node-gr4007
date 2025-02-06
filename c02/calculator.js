5f4b2bc523d9bf60a1c1aec4757a78c5b4e2446a
Vangel22 <h.vangel22@gmail.com>
2025-02-06 20:33:40 +0100

    Save task

---
+++ c02/calculator.js
@@ -0,0 +1,11 @@
+// Lokalen modul
+
+const MAX_NUMBER = 99;
+
+function calculator(operacija, a, b) {
+  // op = "sobiranje"
+  // op = "+"
+  if (operacija === "sobiranje") return a + b;
+}
+
+// module.exports = calculator; // na ovoj nacin davame pristap na drugite fajlovi do ovaa funkcija
---
+++ c02/index.js
@@ -0,0 +1,22 @@
+// require("")
+// CommonJS -> Node -> synchronous -> module.exports so require("")
+// ES modules -> async -> import/exports
+
+const calculator = require("./calculator");
+
+// Tri tipovi moduli: Local, Core, Third party
+// const fs = require("fs"); // Core module
+// const express = require("express"); // Third party module - se instaliraat so komandata npm install express
+
+// const obj = {
+//   name: "Semos",
+//   age: 40,
+// };
+
+// console.log(obj.name, obj.age);
+// const { name, age } = obj; // object destructuring
+// console.log(name, age);
+
+// Zadaca 1:
+// 1. Dovrsete ja calculator funkcijata
+// 2. Iskoritete ja vo index.js so site metodi (sobiranje, mnozenje, delenje, odzemanje)
